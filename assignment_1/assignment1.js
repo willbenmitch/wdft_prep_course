b1fa2505cd38be5a0d481ffe94f16290d0b47e39
willbenmitch <ben.mitchell01@gmail.com>
2017-03-11 22:19:56 -0500

    assignment #1 and optional exercises

--- assignment_1/assignment1.js
+++ assignment_1/assignment1.js
@@ -2,7 +2,7 @@
 Exercise #1
 Initialize an empty array below in the variable numbers.
 */
-var numbers;
+var numbers = [];
 console.log('hello world');
 
 /*
@@ -12,6 +12,7 @@ Using the loop provided for you below, use an array's push method to insert the 
 
 for(var i=0;i<10;i++){
     //use the array push() method here.
+numbers.push(i);
 }
 
 
@@ -21,7 +22,9 @@ Test that you used the array push method correctly by printing the sum of the va
 The outputted value should be 9.
 */
 
+var exercise3 = numbers[3] + numbers[6];
 
+console.log(exercise3);
 
 
 /*
@@ -30,11 +33,11 @@ Use the console.log() function and the array .pop() to add all the values in the
 */
 
 var sum = 0;
-/* remove this line to uncomment the code block
+
 while(numbers.length != 0){
-    //write pop function here
+    sum = sum + numbers.pop()
 }
-remove this line to uncomment the code block */
+
 console.log(sum);
 
 
@@ -42,12 +45,14 @@ console.log(sum);
 Exercise #5
 Create an empty object and assign it the variable car below.
 */
-var car;
+var car = {};
 
 /*
 Exercise #5
 Give the car object a property called "colour" and assign it the value of "black". Test that your code works correctly by outputing property make to the console. You should see the value "black" printed out.
 */
 
+car.colour = "black";
 
+console.log(car.colour);
 
---
+++ assignment_1/exercises.js
@@ -0,0 +1,47 @@
+//exercise 1
+
+var name = "Ben";
+
+console.log('Hello',name);
+
+//exercise 2
+
+var x = 1125;
+var answer = x % 2;
+
+console.log(answer);
+
+//exercise 3
+
+var a = true;
+var b = false;
+var c = true;
+var answer3 = a || b || c;
+
+console.log(answer3);
+
+//exercise 4
+
+var answer4 = a && b && c;
+
+console.log(answer4);
+
+//exercise 5
+
+var answer1 = 10 -3 * 5/4 + 202 % 3;
+var answer2 = 10 - (3 * (5/4) + (202 % 3));
+
+console.log(answer1,answer2);
+
+//exercise 6
+
+var convert1 = 12;
+var convert2 = 113;
+var convert3 = 1.12356;
+var x = parseInt("Hello World");
+
+var convert1a = convert1.toString();
+var convert2a = parseInt(convert2);
+var convert3a = parseFloat(convert3);
+
+console.log(convert1a,convert2a,convert3a,x);
